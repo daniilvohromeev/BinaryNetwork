663beee7e1ccb262752be80addf38d46fb84a768
daniil <myps4accaunt2001@gmail.com>
2023-09-15 21:08:00 +0300

    Испралвена работа скрытых слоёв

--- neural_network_main.cpp
+++ neural_network_main.cpp
@@ -2,261 +2,217 @@
 #include <vector>
 #include <cmath>
 #include <ctime>
-#include "stb_image.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-using namespace std;
-// Функция активации (сигмоида).
+#include <cstdlib>
+
+// Функция сигмоидальной активации
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
 
-// Производная функции активации.
+// Производная функции сигмоидальной активации
 double sigmoid_derivative(double x) {
     return x * (1.0 - x);
 }
 
-class NeuralNetwork {
+class Neuron {
 public:
-    NeuralNetwork(int input_size, const std::vector<int>& hidden_layer_sizes, int output_size)
-            : input_size_(input_size), hidden_layer_sizes_(hidden_layer_sizes), output_size_(output_size) {
-        srand(time(0));
-        initializeWeights();
+    Neuron(int num_inputs) {
+        weights.resize(num_inputs);
+        for (int i = 0; i < num_inputs; ++i) {
+            // Инициализация весов случайными значениями
+            weights[i] = static_cast<double>(rand()) / RAND_MAX;
+        }
+        bias = static_cast<double>(rand()) / RAND_MAX;
     }
 
-    // Инициализация весов нейронной сети.
-    void initializeWeights() {
-        int prev_layer_size = input_size_;
-
-        // Инициализируем веса для скрытых слоев.
-        for (size_t i = 0; i < hidden_layer_sizes_.size(); ++i) {
-            int current_layer_size = hidden_layer_sizes_[i];
-            std::vector<std::vector<double>> layer_weights(current_layer_size, std::vector<double>(prev_layer_size));
-            hidden_weights_.push_back(layer_weights);
-            prev_layer_size = current_layer_size;
+    double feedforward(const std::vector<double>& inputs) {
+        double sum = bias;
+        for (int i = 0; i < inputs.size(); ++i) {
+            sum += inputs[i] * weights[i];
         }
-
-        // Инициализируем веса для выходного слоя.
-        output_weights_.resize(output_size_, std::vector<double>(prev_layer_size));
-
-        // Инициализируем биасы.
-        hidden_biases_.resize(hidden_layer_sizes_.size(), 1.0);
-        output_bias_ = 1.0;
-
-        // Заполняем веса случайными значениями.
-        for (size_t i = 0; i < hidden_weights_.size(); ++i) {
-            for (int j = 0; j < hidden_layer_sizes_[i]; ++j) {
-                for (int k = 0; k < prev_layer_size; ++k) {
-                    hidden_weights_[i][j][k] = (double)rand() / RAND_MAX;
-                }
-            }
-        }
-
-        for (int i = 0; i < output_size_; ++i) {
-            for (int j = 0; j < prev_layer_size; ++j) {
-                output_weights_[i][j] = (double)rand() / RAND_MAX;
-            }
-        }
-    }
-
-    // Обучение нейронной сети методом обратного распространения ошибки.
-    void train(const std::vector<double>& input_data, const std::vector<double>& target) {
-        // Прямое распространение (подсчет выхода сети).
-        std::vector<std::vector<double>> layer_outputs;
-        std::vector<double> layer_input = input_data;
-
-        for (size_t i = 0; i < hidden_weights_.size(); ++i) {
-            std::vector<double> layer_output(hidden_layer_sizes_[i]);
-            for (int j = 0; j < hidden_layer_sizes_[i]; ++j) {
-                double weighted_sum = 0;
-                for (int k = 0; k < input_size_; ++k) {
-                    weighted_sum += layer_input[k] * hidden_weights_[i][j][k];
-                }
-                weighted_sum += hidden_biases_[i];
-                layer_output[j] = sigmoid(weighted_sum);
-            }
-            layer_outputs.push_back(layer_output);
-            layer_input = layer_output;
-        }
-
-        std::vector<double> output(output_size_);
-        for (int i = 0; i < output_size_; ++i) {
-            double weighted_sum = 0;
-            for (int j = 0; j < hidden_layer_sizes_.back(); ++j) {
-                weighted_sum += layer_input[j] * output_weights_[i][j];
-            }
-            weighted_sum += output_bias_;
-            output[i] = sigmoid(weighted_sum);
-        }
-
-        // Вычисление ошибки.
-        std::vector<double> output_errors(output_size_);
-        for (int i = 0; i < output_size_; ++i) {
-            output_errors[i] = target[i] - output[i];
-        }
-
-        // Обратное распространение ошибки и обновление весов.
-        std::vector<double> delta(output_size_);
-        for (int i = 0; i < output_size_; ++i) {
-            delta[i] = output_errors[i] * sigmoid_derivative(output[i]);
-        }
-
-        for (int i = hidden_weights_.size() - 1; i >= 0; --i) {
-            std::vector<double> next_delta(hidden_layer_sizes_[i]);
-            for (int j = 0; j < hidden_layer_sizes_[i]; ++j) {
-                double error_sum = 0;
-                for (int k = 0; k < output_size_; ++k) {
-                    error_sum += delta[k] * output_weights_[k][j];
-                }
-                next_delta[j] = error_sum * sigmoid_derivative(layer_outputs[i][j]);
-            }
-
-            for (int j = 0; j < hidden_layer_sizes_[i]; ++j) {
-                for (int k = 0; k < input_size_; ++k) {
-                    hidden_weights_[i][j][k] += learning_rate_ * next_delta[j] * input_data[k];
-                }
-            }
-
-            delta = next_delta;
-        }
-
-        for (int i = 0; i < output_size_; ++i) {
-            for (int j = 0; j < hidden_layer_sizes_.back(); ++j) {
-                output_weights_[i][j] += learning_rate_ * delta[i] * layer_outputs.back()[j];
-            }
-        }
-    }
-
-    // Предсказание класса на основе входных данных.
-    std::vector<double> predict(const std::vector<double>& input_data) {
-        std::vector<double> layer_input = input_data;
-        for (size_t i = 0; i < hidden_weights_.size(); ++i) {
-            std::vector<double> layer_output(hidden_layer_sizes_[i]);
-            for (int j = 0; j < hidden_layer_sizes_[i]; ++j) {
-                double weighted_sum = 0;
-                for (int k = 0; k < input_size_; ++k) {
-                    weighted_sum += layer_input[k] * hidden_weights_[i][j][k];
-                }
-                weighted_sum += hidden_biases_[i];
-                layer_output[j] = sigmoid(weighted_sum);
-            }
-            layer_input = layer_output;
-        }
-
-        std::vector<double> output(output_size_);
-        for (int i = 0; i < output_size_; ++i) {
-            double weighted_sum = 0;
-            for (int j = 0; j < hidden_layer_sizes_.back(); ++j) {
-                weighted_sum += layer_input[j] * output_weights_[i][j];
-            }
-            weighted_sum += output_bias_;
-            output[i] = sigmoid(weighted_sum);
-        }
-
+        output = sigmoid(sum);
         return output;
     }
 
-private:
-    int input_size_;
-    std::vector<int> hidden_layer_sizes_;
-    int output_size_;
-
-    std::vector<std::vector<std::vector<double>>> hidden_weights_;
-    std::vector<std::vector<double>> output_weights_;
-
-    std::vector<double> hidden_biases_;
-    double output_bias_;
-
-    const double learning_rate_ = 0.1;
-};
-
-// Функция для преобразования черно-белого изображения в матрицу 7x7
-vector<vector<double>> imageToMatrix(const unsigned char* image, int width, int height) {
-    vector<vector<double>> result(7, vector<double>(7, 0.0)); // Создаем матрицу 7x7, заполненную нулями
-
-    if (image == nullptr || width != 7 || height != 7) {
-        cerr << "Error: Image size must be 7x7 pixels." << endl;
-        return result;
+    double getOutput() const {
+        return output;
     }
 
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < height; ++j) {
-            // В данном примере черный цвет (0) преобразуется в 1, белый цвет (255) преобразуется в 0
-            result[i][j] = (image[i * width + j] < 200) ? 1.0 : 0.0;
+    double getWeight(int index) const {
+        return weights[index];
+    }
+
+    void updateWeight(int index, double delta) {
+        weights[index] += delta;
+    }
+
+    void updateBias(double delta) {
+        bias += delta;
+    }
+
+private:
+    std::vector<double> weights;
+    double bias;
+    double output;
+};
+
+class NeuralNetwork {
+public:
+    NeuralNetwork(const std::vector<int>& layer_sizes) {
+        if (layer_sizes.size() < 2) {
+            std::cerr << "Error: At least 2 layers are required (input and output layers)." << std::endl;
+            exit(1);
+        }
+
+        for (int i = 1; i < layer_sizes.size(); ++i) {
+            std::vector<Neuron> layer;
+            for (int j = 0; j < layer_sizes[i]; ++j) {
+                int num_inputs = (i == 1) ? layer_sizes[0] : layer_sizes[i - 1];
+                layer.push_back(Neuron(num_inputs));
+            }
+            layers.push_back(layer);
         }
     }
 
-    return result;
-}
+    std::vector<double> predict(const std::vector<double>& inputs) {
+        std::vector<double> outputs;
 
-int main() {
-    //пример загрузки и преобразования изображения в матрицу
-    const char* filename = R"(C:\Users\LEGION\CLionProjects\BinaryNetwork\dataset\O.jpg)"; // Укажите путь к вашему изображению JPEG
+        for (int i = 0; i < layers.size(); ++i) {
+            std::vector<double> layer_outputs;
+            for (int j = 0; j < layers[i].size(); ++j) {
+                if (i == 0) {
+                    layer_outputs.push_back(layers[i][j].feedforward(inputs));
+                } else {
+                    layer_outputs.push_back(layers[i][j].feedforward(outputs));
+                }
+            }
+            outputs = layer_outputs;
+        }
 
-    int width, height, channels;
-    unsigned char* image = stbi_load(filename, &width, &height, &channels, 1); // Загрузка изображения в оттенках серого
-
-    if (image == nullptr) {
-        cerr << "Error: Unable to open image." << endl;
-        return 1; // Ошибка при загрузке изображения
+        return outputs;
     }
 
-    vector<vector<double>> imageMatrixTrain = imageToMatrix(image, width, height);
-    //TODO: нужно доработать выгрузку датасета а то я прикинул мы заебемся тут
+    void train(const std::vector<std::vector<double>>& input_data, const std::vector<std::vector<double>>& target_data, double learning_rate, int num_epochs) {
+        for (int epoch = 0; epoch < num_epochs; ++epoch) {
+            for (int i = 0; i < input_data.size(); ++i) {
+                std::vector<std::vector<double>> layer_inputs(layers.size());
+                std::vector<std::vector<double>> layer_outputs(layers.size());
 
-    // Создаем динамическую нейронную сеть с конфигурацией.
-    NeuralNetwork neural_network(49, {16}, 1);
+                // Прямое распространение
+                for (int j = 0; j < layers.size(); ++j) {
+                    if (j == 0) {
+                        layer_inputs[j] = input_data[i];
+                    } else {
+                        layer_inputs[j] = layer_outputs[j - 1];
+                    }
 
-    // Обучаем нейронную сеть на примерах.
+                    for (int k = 0; k < layers[j].size(); ++k) {
+                        layer_outputs[j].push_back(layers[j][k].feedforward(layer_inputs[j]));
+                    }
+                }
+
+                // Обратное распространение
+                std::vector<std::vector<double>> layer_errors(layers.size());
+                for (int j = layers.size() - 1; j >= 0; --j) {
+                    layer_errors[j].resize(layers[j].size(), 0.0);
+
+                    if (j == layers.size() - 1) {
+                        for (int k = 0; k < layers[j].size(); ++k) {
+                            double error = target_data[i][k] - layer_outputs[j][k];
+                            layer_errors[j][k] = error * sigmoid_derivative(layer_outputs[j][k]);
+                        }
+                    } else {
+                        for (int k = 0; k < layers[j].size(); ++k) {
+                            double error = 0.0;
+                            for (int n = 0; n < layers[j + 1].size(); ++n) {
+                                error += layer_errors[j + 1][n] * layers[j + 1][n].getWeight(k);
+                            }
+                            layer_errors[j][k] = error * sigmoid_derivative(layer_outputs[j][k]);
+                        }
+                    }
+                }
+
+                // Обновление весов и смещений
+                for (int j = 0; j < layers.size(); ++j) {
+                    for (int k = 0; k < layers[j].size(); ++k) {
+                        for (int n = 0; n < layer_inputs[j].size(); ++n) {
+                            double delta = learning_rate * layer_errors[j][k] * layer_inputs[j][n];
+                            layers[j][k].updateWeight(n, delta);
+                        }
+                        double delta = learning_rate * layer_errors[j][k];
+                        layers[j][k].updateBias(delta);
+                    }
+                }
+            }
+        }
+    }
+
+private:
+    std::vector<std::vector<Neuron>> layers;
+};
+
+int main() {
+
     std::vector<std::vector<double>> training_data = {
             {0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0,
-             0, 0, 1, 1, 0, 0, 0,
-             0, 0, 1, 1, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0},
+                    0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 1, 1, 1, 0, 0,
+                    0, 0, 1, 1, 1, 0, 0,
+                    0, 0, 1, 1, 1, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0},
+
 
             {0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0,
-             0, 1, 1, 1, 0, 0, 0,
-             0, 1, 1, 1, 0, 0, 0,
-             0, 1, 1, 1, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0},
+                    0, 0, 1, 1, 1, 0, 0,
+                    0, 1, 0, 0, 0, 1, 0,
+                    0, 1, 0, 0, 0, 1, 0,
+                    0, 1, 0, 0, 0, 1, 0,
+                    0, 0, 1, 1, 1, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0},
 
+            {0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 1, 0, 0, 0,
+                    0, 0, 1, 0, 1, 0, 0,
+                    0, 1, 1, 1, 1, 1, 0,
+                    0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0},
 
+            // здесь другие примеры для обучения сети.
+    };
+    std::vector<std::vector<double>> test_data = {
             {0, 0, 0, 0, 0, 0, 0,
              0, 0, 0, 0, 0, 0, 0,
              0, 0, 0, 1, 0, 0, 0,
              0, 0, 1, 1, 1, 0, 0,
              0, 1, 1, 1, 1, 1, 0,
-             0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 1, 0, 0, 0,
              0, 0, 0, 0, 0, 0, 0},
-
-            // здесь другие примеры для обучения сети.
     };
+    std::vector<std::vector<double>> targets = {{1,0,0}, {0,1,0}, {0,0,1}};
+    // Инициализация генератора случайных чисел
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    std::vector<std::vector<double>> targets = {{1}, {1}, {0}}; // соответствующие метки классов.
+    // Пример данных для обучения и тестирования
+    std::vector<std::vector<double>> input_data = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    std::vector<std::vector<double>> target_data = {{0,1}, {1,0}, {1,0}, {0,1}};
 
-    for (int epoch = 0; epoch < 10000; ++epoch) { // Обучение на 10000 эпох.
-        for (size_t i = 0; i < training_data.size(); ++i) {
-            neural_network.train(training_data[i], targets[i]);
-        }
+    std::vector<int> layer_sizes = {49, 16, 16, 3}; // Количество нейронов в каждом слое
+
+    NeuralNetwork nn(layer_sizes);
+
+    double learning_rate = 0.5;
+    int num_epochs = 10000;
+
+    nn.train(training_data, targets, learning_rate, num_epochs);
+
+    // Тестирование
+    for (const auto& input : test_data) {
+        std::vector<double> predicted = nn.predict(input);
+        std::cout << "Input: {" << input[0] << ", " << input[1] << "} ";
+        std::cout << "Output: " << predicted[0] <<"/"<< predicted[1] << "/" << predicted[2] << std::endl;
     }
 
-    // Пример предсказания класса для новых данных.
-    std::vector<double> new_data = {0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 1, 1, 1,
-                                    0, 0, 0, 0, 1, 1, 1,
-                                    0, 0, 0, 0, 1, 1, 1};
-
-    std::vector<double> prediction = neural_network.predict(new_data);
-    for (auto val: prediction) {
-        std::cout << "Predict_output: " << val << std::endl;
-    }
     return 0;
 }
